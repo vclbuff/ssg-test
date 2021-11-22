20dca47d63cee73da7928ba62eab96eb87aaf515
vclbuff <vclbuff@gmail.com>
2021-11-22 21:06:07 +0900

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    カウンター {{ counter }}
+    <button @click="inc">
+      プラス
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    inc () {
+      this.counter = this.counter + 1
+    }
+  }
+}
+</script>
